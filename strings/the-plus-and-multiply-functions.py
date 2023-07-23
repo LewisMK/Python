3b141992e83a8c55dd8726c69d9eb5333cb10e72
LewisMK <79156165+LewisMK@users.noreply.github.com>
2023-07-23 22:14:32 +0300

    Update the-plus-and-multiply-functions.py
    Added one more example

--- strings/the-plus-and-multiply-functions.py
+++ strings/the-plus-and-multiply-functions.py
@@ -10,5 +10,14 @@ decoration = (asterisk + plus) * 10 + asterisk
 
 print(decoration)
 
+# Another example
 
+firstName = "John"
+lastName = "Doe"
+
+print(firstName + lastName)
+
+# We can put the name in the middle of the decoration
+
+print(decoration)
 
