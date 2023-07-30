7ffe7ca1b9a08b64bda528798f037201a81ba2f3
LewisMK <79156165+LewisMK@users.noreply.github.com>
2023-07-30 23:27:24 +0300

    Update lambda-cube.py

--- lambda/lambda-cube.py
+++ lambda/lambda-cube.py
@@ -11,3 +11,6 @@ print("Using function with 'def' keyword, cube:", cube(5))
 #using the lambda function
 
 print("Using lambda function, cube:", lambda_cube(5))
+
+
+# here we are using two different functions for same result. Note how the lambda function is short and ideal for simple functions which return one value.
