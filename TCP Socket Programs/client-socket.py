069eaab539c689d450fa5d5126939befe0da82c2
LewisMK <79156165+LewisMK@users.noreply.github.com>
2023-07-30 23:24:22 +0300

    Update client-socket.py

--- TCP Socket Programs/client-socket.py
+++ TCP Socket Programs/client-socket.py
@@ -6,10 +6,10 @@ s = socket.socket()
 
 host = socket.gethostname()  # this command will get the host name where this script is running from
 
-port = 9999
+port = 9999                   # the target port has to match the server's listening port
 
 s.connect((host, port))
 
-print(s.recv(1024))
+print(s.recv(1024))          # 1024 bytes is the TCP maximum message size (MSS). It is usually 1460 bytes after removing TCP and IP header length from the MTU
 
 s.close()
