600721ea405af212dbb0983e8eac517474124379
LewisMK <79156165+LewisMK@users.noreply.github.com>
2023-08-03 22:25:08 +0300

    Update PDFpass.py

--- PDFpass.py
+++ PDFpass.py
@@ -1,11 +1,11 @@
 import pikepdf
 from tqdm import tqdm
 
-#load password list
+# load the password list from local storage
 
 passwords = [ line.strip() for line in open("fasttrack.txt") ]
 
-#iterate over passwords
+# iterate over passwords
 
 for password in tqdm(passwords, "Decrypting PDF"):
 
@@ -13,13 +13,13 @@ for password in tqdm(passwords, "Decrypting PDF"):
         #open PDF file
         
         with pikepdf.open("hello", password=password) as pdf:
-            #password decrypted successfully, break out of the loop
+            # if the password is decrypted successfully, break out of the loop
 
             print("[+] Password found:", password)
             break
 
     except pikepdf._core.PasswordError as e:
-        #wrong password, just continue the loop
+        # wrong password, just continue the loop
 
         continue
     
