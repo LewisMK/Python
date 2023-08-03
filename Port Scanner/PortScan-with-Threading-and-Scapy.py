36353f22adba5b6a051dee0955a2d09045849a68
LewisMK <79156165+LewisMK@users.noreply.github.com>
2023-08-03 22:22:42 +0300

    Update PortScan-with-Threading-and-Scapy.py

--- Port Scanner/PortScan-with-Threading-and-Scapy.py
+++ Port Scanner/PortScan-with-Threading-and-Scapy.py
@@ -5,16 +5,19 @@ import threading
 
 def syn_scan(target, port):
     
-    src_port = RandShort()
+    src_port = RandShort()     # this function generates a random source port to reduce detection by IPS/IDS
     
+    # Create a TCP packet inside an IP packet
+    # sr1 is a scapy function which sends and receives packets
     response = sr1(IP(dst=target)/TCP(sport=src_port, dport = port, flags = "S"), timeout = 1, verbose = 0)
 
     if response and response.haslayer(TCP):
-        if response[TCP].flags == 0x12:
+        if response[TCP].flags == 0x12: #the SYN-ACK flag
             print(f"Port {port} is open")
             
-            send(IP(dst=target)/TCP(sport=src_port, dport = port, flags = "R"))
-        elif response[TCP].flags == 0x14:
+            send(IP(dst=target)/TCP(sport=src_port, dport = port, flags = "R")) # Send an R flag to close the connection
+            
+        elif response[TCP].flags == 0x14: # the RST-ACK flag
             print(f"Port {port} is closed")
             
             
@@ -32,17 +35,19 @@ def scan_ports(target, start_port, end_port):
 
 
 def main():
+
+    # get the necessary input variables 
     
     target = input("Enter the target IP address or hostname: ")
     port_range = input("Enter the port range to scan (e.g 1-100): ")
     dns_domain = input("Enter the domain for DNS scanning: ")
     
     
-    start_port, end_port = map(int, port_range.split("-"))
+    start_port, end_port = map(int, port_range.split("-"))   # turn the input string into a list and convert the items into ints
     
     print(f"Scanning {target} for open ports in range {start_port}-{end_port}:")
     
-    threads = []
+    threads = []      # we want to increase the scanning speed through threading
     
     num_threads = 10
     chunk_size = (end_port - start_port + 1) // num_threads
