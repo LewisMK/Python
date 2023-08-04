d568f5589968a249836842e7b2ce6da1281bdda6
LewisMK <79156165+LewisMK@users.noreply.github.com>
2023-08-04 23:06:50 +0300

    Update performance-caching.py

--- Decorators/performance-caching.py
+++ Decorators/performance-caching.py
@@ -1,7 +1,11 @@
-from functools import lru_cache
+# this program demonstrates the use of decorators for caching
+# the fib sequence can take a lot of time when using large numbers as args
+# thus, the lru_cache decorator is used to improve processing speed dramatically
+
+from functools import lru_cache      # import lru_cache from in-built functools
 
 
-@lru_cache(maxsize=None) 
+@lru_cache(maxsize=None)             # use the lru_cache function as a decorator. The maxsize specifies the number of saved(cached) instances before deletion
 def fib(n):
     
     if n <= 1:
@@ -14,7 +18,9 @@ def fib(n):
 print(fib(10))
 print(fib(50))
 print(fib(100))
-print(fib(500))
+print(fib(500))       
 
+# all functions take less than one second to complete
+# without lru_cache, it would take forever
 
 
