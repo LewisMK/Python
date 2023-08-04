8c4f09c58d1c26c72ae8df6fc883effbad37cdc3
LewisMK <79156165+LewisMK@users.noreply.github.com>
2023-08-04 23:10:16 +0300

    Update performance-logging.py

--- Decorators/performance-logging.py
+++ Decorators/performance-logging.py
@@ -1,10 +1,13 @@
+# this program logs a function's parameters and the output using a decorator
+# logging helps to debug and understand program logic
+
 import logging
 
 def function_logger(func):
     
-    logging.basicConfig(level= logging.INFO, filename= "addition.log")
+    logging.basicConfig(level= logging.INFO, filename= "addition.log")    # open the addition.log file to see the log output
     
-    def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs):       # *args and **kwargs means that the function takes any number of args and kwargs. 
         
         result = func(*args, **kwargs)
         
@@ -16,12 +19,12 @@ def function_logger(func):
 
 
 
-@function_logger
+@function_logger                        # calling the logging function before executing the real function
 def add_one(value):
     
     return value + 1
 
-print(add_one(1))
+print(add_one(1))                       # output is 2
 
 
     
