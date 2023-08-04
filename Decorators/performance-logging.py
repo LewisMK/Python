7a3b4c70649109b2777b901979c3b66ba5c0f67e
LewisMK <elmurimi@gmail.com>
2023-08-04 23:23:45 +0300

    Modified the logging to include execution time

--- Decorators/performance-logging.py
+++ Decorators/performance-logging.py
@@ -1,4 +1,5 @@
 import logging
+import time
 
 def function_logger(func):
     
@@ -6,9 +7,13 @@ def function_logger(func):
     
     def wrapper(*args, **kwargs):
         
+        start_time = time.time()
         result = func(*args, **kwargs)
+        end_time = time.time()
         
-        logging.info(f"{func.__name__}' ran with positional arguments: {args} and keyword arguments: {kwargs}. Return value: {result}")
+        execution_time = end_time - start_time
+        
+        logging.info(f"{func.__name__} ran with positional arguments: {args} and keyword arguments: {kwargs}. Return value: {result}. Total execution time: {execution_time: .6f} seconds")
         
         return result
     
@@ -21,7 +26,7 @@ def add_one(value):
     
     return value + 1
 
-print(add_one(1))
+print(add_one(10))
 
 
     
